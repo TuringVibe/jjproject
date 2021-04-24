534dbae4575181c66e4af0cac6c5e6fc3569cb90
Muhammad Zullidar <zullidar91@gmail.com>
2021-04-24 10:48:28 +0700

    penambahan source code bootstrap-4.6 pada public/lib dan update urlnya

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,7 @@
     <title>Joson Project Management</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700;900&display=swap">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('lib/bootstrap-4.6.0-dist/css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{asset('lib/fontawesome-5.15.3/css/fontawesome.min.css')}}">
     <link rel="stylesheet" href="{{asset('lib/fontawesome-5.15.3/css/regular.min.css')}}">
     <link rel="stylesheet" href="{{asset('lib/fontawesome-5.15.3/css/brands.min.css')}}">
@@ -25,7 +25,7 @@
     </div>
     @include('layouts.footer')
     <script src="{{asset('lib/jquery-3.6.0.min.js')}}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+    <script src="{{asset('lib/bootstrap-4.6.0-dist/js/bootstrap.bundle.min.js')}}"></script>
     <script src="{{asset('lib/sweetalert2-10.15.7/sweetalert2.all.min.js')}}"></script>
     <script src="{{asset('lib/jQuery-slimScroll-1.3.8/jquery.slimscroll.min.js')}}"></script>
     <script src="{{asset('js/general.js')}}"></script>
